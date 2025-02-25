d7fdd7e7efac9f93a30dff33c40b930e59f342e8
affuad1903 <affandi.p5@gmail.com>
2025-02-25 13:25:33 +0700

    Commit Tugas Day 12 Intro Laravel Affandi Putra Pradana

---
+++ day 12/sanberBook/resources/views/home.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SanberBook</title>
+</head>
+<body>
+    <header><h1>SanberBook</h1></header>
+    <section id="introSanberBook">
+        <article>
+            <h2>Social Media Developer Santai Berkualitas</h2>
+            <p>Belajar dan Berbagi agar hidup ini semakin santai berkualitas</p>
+        </article>
+    </section>
+    <section id="benefitSanberBook">
+        <article>
+            <h2>Benefit Join di SanberBook</h2>
+            <ul>
+                <li>Mendapatkan motivasi dari sesama developer</li>
+                <li>Sharing knowledge dari para mastah Sanber</li>
+                <li>Dibuat oleh calon web developer terbaik</li>
+            </ul>
+        </article>
+    </section>
+    <section id="joinSanberBook">
+        <article>
+            <h2>Cara Bergabung ke SanberBook</h2>
+            <ol>
+                <li>Mengunjungi Website ini</li>
+                <li>Mendaftar di <a href="{{url('/register')}}">Form Sign Up</a></li>
+                <li>Selesai!</li>
+            </ol>
+        </article>
+    </section>
+</body>
+</html>
---
+++ day 12/sanberBook/resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+</head>
+<body>
+    <header><h1>SELAMAT DATANG {{$firstName}} {{$lastName}} </h1></header>
+    <section>
+        <article><h2>Terima kasih telah bergabung di SanberBook. Social Media kita bersama!</h2></article>
+    </section>
+</body>
+</html>
